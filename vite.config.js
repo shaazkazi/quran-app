5b77f9bcd83e6f176d4940c8efca1500577d1d8c
shaazkazi <n3tn1nj4@proton.me>
2025-01-25 15:49:24 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'quran-data': [
+            './src/json/arabic.json',
+            './src/json/en.sahih.json',
+            './src/json/ur.maududi.json'
+          ],
+          'vendor': [
+            'react',
+            'react-dom'
+          ]
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
